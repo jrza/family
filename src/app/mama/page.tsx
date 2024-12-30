1e8c157b177652218980dd51a85f46cb876e0bd0
jrza <jazibwaqarr@gmail.com>
2024-12-30 10:35:56 -0600

    fixxxx

--- src/app/mama/page.tsx
+++ src/app/mama/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-interface PageProps {
-  params: {
-    name: string;
-  };
+import { NextPage } from 'next';
+
+interface PageParams {
+  name: string;
 }
 
-export default function Page({ params }: PageProps) {
+const Page: NextPage<{ params: PageParams }> = ({ params }) => {
   return (
     <div>
       <h1>Hello {params.name}</h1>
     </div>
   );
-} 
+};
+
+export default Page; 
